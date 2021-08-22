082195142ed253a22ad0813cebfc70645c29cfb2
Leonetienne <leonetienne@hotmail.de>
2021-08-22 12:12:23 +0200

    Fixed issue that caused the button to not load

--- ext.js
+++ ext.js
@@ -1,20 +1,31 @@
-window.setTimeout(function() {
-    // Inject 'flip player' button
-    $('#info-strings').append(`
-        <span id="dot" class="style-scope ytd-video-primary-info-renderer"></span>
-        <span id="youtube-flipper" style="cursor: pointer;">Flip player<span>
-    `);
 
-    // Register click handler for 'flip player' button
-    $('#youtube-flipper').click(function() {
-        // Fetch the youtube watch felxy
-        youtubeFlexy = $('div#columns.style-scope.ytd-watch-flexy');
+// Injects the flip button if the player element is already loaded, and the button is not already there
+function CheckInjectButton() {
+    if (($('#youtube-flipper').length == 0) && ($('#info-strings').length > 0)) {
 
-        // Toggle css attribute 'flex-direction' between 'row' and 'row-reverse'
-        if (youtubeFlexy.css('flex-direction') == 'row')
-            youtubeFlexy.css('flex-direction', 'row-reverse');
-        else
-            youtubeFlexy.css('flex-direction', 'row');
-    });
+        // Inject button html
+        $('#info-strings').append(`
+            <span id="dot" class="style-scope ytd-video-primary-info-renderer"></span>
+            <span id="youtube-flipper" style="cursor: pointer;">Flip player<span>
+        `);
 
-}, 1000);
+        // Register click handler for 'flip player' button
+        $('#youtube-flipper').click(function() {
+            // Fetch the youtube watch felxy
+            youtubeFlexy = $('div#columns.style-scope.ytd-watch-flexy');
+
+            // Toggle css attribute 'flex-direction' between 'row' and 'row-reverse'
+            if (youtubeFlexy.css('flex-direction') == 'row')
+                youtubeFlexy.css('flex-direction', 'row-reverse');
+            else
+                youtubeFlexy.css('flex-direction', 'row');
+        });
+    }
+}
+
+// Check if the player element has been loaded at a few timepoints
+window.setTimeout(CheckInjectButton, 200);
+window.setTimeout(CheckInjectButton, 500);
+window.setTimeout(CheckInjectButton, 1000);
+window.setTimeout(CheckInjectButton, 5000);
+window.setTimeout(CheckInjectButton, 10000);
